f8c9df61bd3f91d2f11704f30758b278deafecc6
Minh-Quan TRAN <qtran@ijenko.com>
2016-02-18 17:29:31 +0100

    kadira

--- server/lib/environment.js
+++ server/lib/environment.js
@@ -1,7 +1,6 @@
 Meteor.startup(function(){
   if(process.env.METEOR_ENV =="production") {
     console.log("Running in production");
-    process.env.ROOT_URL="http://your-url" //production url
+    process.env.ROOT_URL="http://itscaro.me" //production url
   }
-
 });
---
+++ server/lib/kadira.js
@@ -0,0 +1,1 @@
+Kadira.connect('tCvH6BBzaeD9Ch7ZF', 'd056b0de-10c6-44dd-bd40-e56036701f12');
